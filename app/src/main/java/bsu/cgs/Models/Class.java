a0edcbcc21a91b09e43ab6e712a0f5f64505ff8b
ellej16 <pjinxed.aranzaellej@gmail.com>
2015-05-21 12:57:56 +0800

    Add Database models

---
+++ app/src/main/java/bsu/cgs/Models/Class.java
@@ -0,0 +1,11 @@
+package bsu.cgs.Models;
+
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+
+/*
+ * Created by Paul justin on 05/21/2015.
+ */
+public class Class {
+}

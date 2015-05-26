02c3e9a589a872035ba0e610a8ec30ce1dfcc46b
ellej16 <pjinxed.aranzaellej@gmail.com>
2015-05-26 23:09:20 +0800

    @Student_create.java: Remove unused imports, add UI components, Student creation functional

--- app/src/main/java/bsu/cgs/student_create.java
+++ app/src/main/java/bsu/cgs/student_create.java
@@ -1,49 +1,39 @@
 package bsu.cgs;
 
 import android.app.Activity;
-import android.net.Uri;
+import android.content.Context;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import bsu.cgs.Models.*;
+import bsu.cgs.Models.Student;
+
+import com.activeandroid.query.Select;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link student_create.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link student_create#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class student_create extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment student_create.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static student_create newInstance(String param1, String param2) {
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    EditText studNum;
+    EditText studName;
+    Button btnSave;
+    Toast toast;
+    public static student_create newInstance(int sectionNumber)
+    {
         student_create fragment = new student_create();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+
         return fragment;
     }
 
@@ -55,55 +45,85 @@ public class student_create extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+        //TODO : remove all empty if statements , unused imports and etc.
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_student_create, container, false);
+        View createStudView =inflater.inflate(R.layout.fragment_student_create, container, false);
+
+        studName = (EditText) createStudView.findViewById(R.id.tbStudName);
+        studNum = (EditText) createStudView.findViewById(R.id.tbSnum);
+        btnSave = (Button) createStudView.findViewById(R.id.btnStudSave);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                save();
+            }
+        });
+        return createStudView;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
-        try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
+
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
+    public void save()
+    {
+        Student stud;
+        if(studName.getText().length()==0 || studNum.getText().length() ==0 )
+        {
+            Context context = getActivity().getApplicationContext();
+            CharSequence text = "No fields should be empty!";
+            int duration = Toast.LENGTH_SHORT;
+
+            toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+        else
+        {
+
+            stud = new Select()
+                    .from(Student.class)
+                    .where("snum=?", studNum.getText())
+                    .orderBy("RANDOM()")
+                    .executeSingle();
+            if (stud != null) {
+                Context context = getActivity().getApplicationContext();
+                CharSequence text = "Subject code already exists!";
+                int duration = Toast.LENGTH_SHORT;
+
+                toast = Toast.makeText(context, text, duration);
+                toast.show();
+            } else
+            {
+                Student newStud = new Student();
+                newStud.studName = studName.getText().toString();
+                newStud.studNum = studNum.getText().toString();
+                newStud.save();
+                Context context = getActivity().getApplicationContext();
+                CharSequence text = "Student added!";
+                studNum.setText("");
+                studName.setText("");
+                int duration = Toast.LENGTH_SHORT;
+
+                toast = Toast.makeText(context, text, duration);
+                toast.show();
+
+            }
+        }
     }
+
 
 }

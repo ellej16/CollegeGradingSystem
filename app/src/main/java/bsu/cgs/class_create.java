c78501b41a4bccea0f647cf13606abe1f3c3422d
ellej16 <pjinxed.aranzaellej@gmail.com>
2015-05-27 00:50:18 +0800

    Add onclick listeners and dummy functions for class creator component

--- app/src/main/java/bsu/cgs/class_create.java
+++ app/src/main/java/bsu/cgs/class_create.java
@@ -7,43 +7,36 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link class_create.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link class_create#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class class_create extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment class_create.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static class_create newInstance(String param1, String param2) {
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+
+    Button btnMngStuds;
+    Button btnMngCrit;
+    Button btnSave;
+
+    TextView lblSubjCode;
+    TextView lblSubjName;
+    TextView lblStudCount;
+    TextView lblCritCount;
+
+    EditText tbCname;
+
+    public static class_create newInstance(int sectionNumber) {
         class_create fragment = new class_create();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+
         fragment.setArguments(args);
+
         return fragment;
     }
 
@@ -55,16 +48,54 @@ public class class_create extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_class_create, container, false);
+        View classCreateView =inflater.inflate(R.layout.fragment_class_create, container, false);
+
+        btnMngStuds = (Button) classCreateView.findViewById(R.id.btnMngStuds);
+        btnMngStuds.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.onSelectOption(getArguments().getInt(ARG_SECTION_NUMBER),1);
+            }
+        });
+
+        btnMngCrit = (Button) classCreateView.findViewById(R.id.btnMngCrit);
+        btnMngCrit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.onSelectOption(getArguments().getInt(ARG_SECTION_NUMBER),2);
+            }
+        });
+
+        btnSave = (Button) classCreateView.findViewById(R.id.btnCcrtSave);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                save();
+            }
+        });
+
+        lblSubjCode = (TextView) classCreateView.findViewById(R.id.lblSubjCode);
+        lblSubjName= (TextView) classCreateView.findViewById(R.id.lblSelSubj);
+        lblSubjName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mListener.onSelectOption(getArguments().getInt(ARG_SECTION_NUMBER),0);
+            }
+        });
+        lblStudCount= (TextView) classCreateView.findViewById(R.id.lblStudCount);
+        lblCritCount= (TextView) classCreateView.findViewById(R.id.lblCrit);
+        tbCname = (EditText) classCreateView.findViewById(R.id.tbCname);
+
+
+
+
+        return classCreateView;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -91,16 +122,6 @@ public class class_create extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         public void onFragmentInteraction(Uri uri);

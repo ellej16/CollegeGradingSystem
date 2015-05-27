4310088d3fcbf1ba3729bd1fe6d5170c211a02b2
ellej16 <pjinxed.aranzaellej@gmail.com>
2015-05-27 21:08:03 +0800

    Implement subject select function

---
+++ app/src/main/java/bsu/cgs/class_subject_select.java
@@ -0,0 +1,151 @@
+package bsu.cgs;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+
+import com.activeandroid.query.Select;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import bsu.cgs.Models.*;
+import bsu.cgs.Models.Subject;
+import bsu.cgs.dummy.DummyContent;
+
+/**
+ * A fragment representing a list of Items.
+ * <p/>
+ * Large screen devices (such as tablets) are supported by replacing the ListView
+ * with a GridView.
+ * <p/>
+ * Activities containing this fragment MUST implement the {@link OnFragmentInteractionListener}
+ * interface.
+ */
+public class class_subject_select extends Fragment implements AbsListView.OnItemClickListener {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_SECTION_NUMBER = "section_number";
+    private OnFragmentInteractionListener mListener;
+
+    /**
+     * The fragment's ListView/GridView.
+     */
+    private AbsListView mListView;
+
+    /**
+     * The Adapter which will be used to populate the ListView/GridView with
+     * Views.
+     */
+    private ListAdapter mAdapter;
+
+    public static class_subject_select newInstance(int sectionNumber) {
+        class_subject_select fragment = new class_subject_select();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public class_subject_select() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+        }
+
+        mAdapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1, android.R.id.text1, getContent());
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_class_subject_select, container, false);
+
+        // Set the adapter
+        mListView = (AbsListView) view.findViewById(android.R.id.list);
+        ((AdapterView<ListAdapter>) mListView).setAdapter(mAdapter);
+
+
+        mListView.setOnItemClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+        ((MainActivity) activity).onSectionAttached(
+                getArguments().getInt(ARG_SECTION_NUMBER));
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        if (null != mListener) {
+
+
+                mListener.onSelectSubj(mListView.getItemAtPosition(position).toString(),
+                getArguments().getInt(ARG_SECTION_NUMBER));
+        }
+    }
+
+    /**
+     * The default content for this Fragment has a TextView that is shown when
+     * the list is empty. If you would like to change the text, call this method
+     * to supply the text it should use.
+     */
+    public void setEmptyText(CharSequence emptyText) {
+        View emptyView = mListView.getEmptyView();
+
+        if (emptyView instanceof TextView) {
+            ((TextView) emptyView).setText(emptyText);
+        }
+    }
+
+    public List<String> getContent()
+    {
+        List<String> toReturn = new ArrayList<String>();
+        List<Subject> Subjects = new Select()
+                .from(Subject.class)
+                .orderBy("subjcode ASC")
+                .execute();
+
+        for(Subject sub : Subjects)
+        {
+            toReturn.add(sub.subjcode+":"+sub.description);
+        }
+        return toReturn;
+    }
+    public interface OnFragmentInteractionListener {
+
+        public void onSelectSubj(String subjcode,int section);
+    }
+
+}

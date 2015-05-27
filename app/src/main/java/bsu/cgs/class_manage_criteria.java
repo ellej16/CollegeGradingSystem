9822464b6f85227b2396ec1ec76ab826d7565072
ellej16 <pjinxed.aranzaellej@gmail.com>
2015-05-28 03:58:26 +0800

    update manage criteria

--- app/src/main/java/bsu/cgs/class_manage_criteria.java
+++ app/src/main/java/bsu/cgs/class_manage_criteria.java
@@ -1,48 +1,54 @@
 package bsu.cgs;
 
 import android.app.Activity;
+import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link class_manage_criteria.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link class_manage_criteria#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class class_manage_criteria extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
+    private static final String ARG_SECTION_NUMBER = "section_number";
 
     private OnFragmentInteractionListener mListener;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment class_manage_criteria.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static class_manage_criteria newInstance(String param1, String param2) {
+
+    private ListView critListview;
+    private ArrayAdapter critAdapter;
+
+    public List<String> critList =  new ArrayList<String>();
+
+    Button addCrit;
+    Button remCrit;
+    Button saveCrits;
+
+    EditText tbCritName;
+    EditText tbPercent;
+    EditText tbRecs;
+
+    Toast toast;
+    CharSequence text;
+
+    public static class_manage_criteria newInstance(int sectionNumber) {
         class_manage_criteria fragment = new class_manage_criteria();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -55,24 +61,55 @@ public class class_manage_criteria extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_class_manage_criteria, container, false);
+        View critManageView = inflater.inflate(R.layout.fragment_class_manage_criteria,
+                container, false);
+
+
+        tbCritName = (EditText) critManageView.findViewById(R.id.tbCritName);
+        tbPercent = (EditText) critManageView.findViewById(R.id.tbPercent);
+        tbRecs = (EditText) critManageView.findViewById(R.id.tbRecCount);
+
+
+
+
+        addCrit = (Button) critManageView.findViewById(R.id.btnAddCrit);
+        addCrit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dagdagCrit();
+            }
+        });
+        remCrit = (Button) critManageView.findViewById(R.id.btnRemCrit);
+        remCrit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                removeCrit();
+            }
+        });
+        saveCrits = (Button) critManageView.findViewById(R.id.btnCritSave);
+        saveCrits.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                save();
+            }
+        });
+        critListview = (ListView) critManageView.findViewById(R.id.critList);
+        critListview.setAdapter(critAdapter);
+        critAdapter = new ArrayAdapter<String>( getActivity()
+        , android.R.layout.simple_list_item_multiple_choice,android.R.id.text1,critList);
+
+
+        return critManageView;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+
 
     @Override
     public void onAttach(Activity activity) {
@@ -91,19 +128,77 @@ public class class_manage_criteria extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+   public void dagdagCrit()
+   {
+       if(tbRecs.getText().length()==0
+               || tbPercent.getText().length()==0
+               || tbCritName.getText().length()==0)
+       {
+           Context context = getActivity().getApplicationContext();
+           text = "No fields should be empty!";
+           int duration = Toast.LENGTH_SHORT;
+
+           toast = Toast.makeText(context, text, duration);
+           toast.show();
+       }
+       else if((Integer.parseInt(tbRecs.getText().toString()) <= 0 ||
+               Double.parseDouble(tbPercent.getText().toString())<=0))
+       {
+           Context context = getActivity().getApplicationContext();
+           text = "Number of records/Percentage should not be 0!";
+           int duration = Toast.LENGTH_SHORT;
+
+           toast = Toast.makeText(context, text, duration);
+           toast.show();
+
+       }
+       else
+       {
+
+
+
+           critAdapter.add(tbCritName.getText().toString()+
+                   ":"+tbPercent.getText().toString()+" %:"+tbRecs.getText().toString()+" records");
+           Context context = getActivity().getApplicationContext();
+           text = tbCritName.getText().toString()+
+                   ":"+tbPercent.getText()+"%:"+tbRecs.getText().toString()+" records";
+           int duration = Toast.LENGTH_SHORT;
+
+           critAdapter.notifyDataSetChanged();
+           toast = Toast.makeText(context, text, duration);
+           toast.show();
+
+
+       }
+
+   }
+
+    public void removeCrit()
+    {
+        SparseBooleanArray studList  = critListview.getCheckedItemPositions();
+        for(int i = 0; i < critListview.getCount(); i++)
+            if (studList.get(i))
+            {
+                Object item = critAdapter.getItem(i);
+                critAdapter.remove(item);
+            }
+        critAdapter.notifyDataSetChanged();
+        Context context = getActivity().getApplicationContext();
+        text = "Criteria removed!";
+        int duration = Toast.LENGTH_SHORT;
+        critAdapter.notifyDataSetChanged();
+        toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    public void save()
+    {
+
+        mListener.onSaveCriteria(getArguments().getInt(ARG_SECTION_NUMBER),critList);
+    }
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
+        public void onSaveCriteria(int section, List<String> criteria);
     }
 
 }
